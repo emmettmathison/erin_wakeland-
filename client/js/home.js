b055a7855e619bed4725e004ef74b183c87af5e7
Crash Override <emmettmathison@gmail.com>
2021-01-08 15:46:31 -0800

    content/structuring

---
+++ client/js/home.js
@@ -0,0 +1,61 @@
+$("#erinWakeland").on("mouseover", function () {
+  $("#erin").show();
+});
+
+$("#oldPage").on("mouseover", function () {
+  $("#oldPageMovie").show();
+});
+
+$("#yarnVideos").on("mouseover", function () {
+  $("#yarnMovie").show();
+});
+
+$("#paint").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#write").on("mouseover", function () {
+  $("#everythingYouNeed").show();
+});
+
+$("#designedThings").on("mouseover", function () {});
+
+$("#photos").on("mouseover", function () {
+  $("#photoFriends").show();
+});
+
+$("#elephant").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#here").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#coolestGig").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#earthBody").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#islandSurvival").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#ancientBathtub").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#surprise").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#surpriseBook").on("mouseover", function () {
+  console.log("hovering");
+});
+
+$("#campPrefer").on("mouseover", function () {
+  console.log("hovering");
+});
